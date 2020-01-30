2a9e43bcf5612cad80e8cb09ac8b470a3d4a4d34
Raymond Healy <rmh2805@g.rit.edu>
2020-01-30 11:25:28 -0500

    I believe this is finished

--- homework1-P/impl.py
+++ homework1-P/impl.py
@@ -36,6 +36,12 @@ def listWords(dictPath):
 
 # Counts the difference in characters between the two provided strings
 def difChars(w1, w2):
+    if isinstance(w1, myNode):
+        w1 = w1.word
+
+    if isinstance(w2, myNode):
+        w2 = w2.word
+
     dif = abs(len(w1) - len(w2))
     for i in range(0, min(len(w1), len(w2))):
         if w1[i] != w2[i]:
@@ -45,7 +51,10 @@ def difChars(w1, w2):
 
 
 # Gives a list of all possible next nodes (1 char changed from the word in)
-def generateNodes(wordIn, words):
+def getActions(wordIn, words):
+    if isinstance(wordIn, myNode):
+        wordIn = wordIn.word
+
     actions = []
     for word in words:
         if difChars(wordIn, word) == 1:
@@ -55,9 +64,8 @@ def generateNodes(wordIn, words):
 
 
 def main():
-    w1 = "small"
-    w2 = "short"
-    dictPath = "exampleWords.txt"
+    (w1, w2) = promptWords()
+    dictPath = promptDict()
     words = listWords(dictPath)
 
     for i in range(0, len(words)):
@@ -69,7 +77,8 @@ def main():
         print("ERROR: " + w2 + " is not in the provided dictionary")
         return 1
 
-    visited = findPath(w1, w2, words)
+    path = findPath(w1, w2, words)
+    print(path)
     return 0
 
 
@@ -77,24 +86,79 @@ class myNode:
     word = ""
     parent = ""
     g = 0
-    f = 0
 
-    def __init__(self, word="", parent="", g=0, f=0):
+    def __init__(self, word="", parent="", g=0):
         self.word = word
         self.parent = parent
         self.g = g
-        self.f = f
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.word == other
+        if isinstance(other, self.__class__):
+            return self.word == other.word
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+def makePath(visitedMap, dest):
+    path = []
+    word = dest
+    while word != "":
+        path.insert(0, word)
+        word = visitedMap[word].parent
+
+    return path
 
 
 # Best first search
-def findPath(w1, w2, words):
-    visited = {}
+def findPath(start, dest, words):
+    # Create a node for the starting point (no parent, no path weight)
+    sNode = myNode(start)
 
-    # Create a node for the starting point
-    myNode(w1, "", 0, difChars(w1, w2))
-    toVisit = [myNode]
+    # Mark the
+    pQueue = [sNode]
+    visited = {start: sNode}
 
-    return visited
+    while len(pQueue) > 0:
+        node = pQueue.pop(0)
+
+        # If the lowest weighted node is our destination, we have found a shortest path
+        if node == dest:
+            return makePath(visited, dest)
+
+        # Get potential next words from `node`'s word
+        # noinspection PyTypeChecker
+        actions = getActions(node, words)
+
+        for action in actions:
+            """ If this action has already been taken:
+                    grab the node generated for it then and then skip to the next action if this route is faster
+                If this is a new action:
+                    generate a new node for this action and mark it as visited
+            """
+            if action in visited:
+                aNode = visited[action]
+                if aNode.g <= node.g + 1:
+                    continue
+            else:
+                aNode = myNode(action)
+                visited[action] = aNode
+
+            # Since this is the fastest known path to aNode, update its path cost and parent
+            aNode.g = node.g + 1
+            aNode.parent = node.word
+
+            # Enqueue this node if it is not already in queue
+            if aNode not in pQueue:
+                pQueue.append(aNode)
+
+        pQueue.sort(key=lambda n: n.g + difChars(n, dest))  # Sort the list by pathCost + heuristic
+
+    # Failed to find a route
+    return "Failure"
 
 
 if __name__ == '__main__':
