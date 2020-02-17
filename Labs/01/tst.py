5cf88e8435daec6127f597d03a1ca927bf925ffb
rmh2805 <rmh2805@g.rit.edu>
2020-02-17 08:53:16 -0500

    Started a new test

--- Labs/01/tst.py
+++ Labs/01/tst.py
@@ -1,5 +1,26 @@
+import math
+
 from PIL import Image
 
 # im = Image.open("Inputs/mpTerrain.png")
 
-print((0xFF, 0xFF, 0xFF) == (255, 255, 255)[0:3])
+horiDist = 1000
+vertDist = 0
+
+rawSlope = vertDist/horiDist
+overallPace = math.e**(3.5 * abs(rawSlope + 0.05))
+
+print(overallPace)
+
+slopes = [0, 1/20, 1, 5, 10, 20, 30, 45, 90]
+
+
+for slope in slopes[1:]:
+    slopes.append(-slope)
+
+
+
+for dHori in range(0, 1000):
+    pass
+
+print(slopes)
