7354ab2c31d487f854c93673dd48d8e2db850703
Raymond Healy <rmh2805@g.rit.edu>
2020-04-19 20:42:22 -0400

    Paranoid checks to see if the weighted tree works properly

--- Labs/02/testDTree.py
+++ Labs/02/testDTree.py
@@ -6,20 +6,32 @@ import pickle
 
 def main():
     dataSet, attrs, attrRanges, categories = readData('Data/classExample.data')
-    tree = weightedDTree()
+    wTree = weightedDTree()
 
-    weight = float(1)/len(dataSet)
+    weight = float(1) / len(dataSet)
     weights = []
     for datum in dataSet:
         weights.append(weight)
 
-    tree.teachWeighted(dataSet, weights, attrs, attrRanges, categories)
+    wTree.teachWeighted(dataSet, weights, attrs, attrRanges, categories)
+
+    tree = dTree()
+    tree.teach(dataSet, attrs, attrRanges, categories)
 
     true = 'True'
     false = 'False'
-    testDict = {'Horn': false, 'Flute': true, 'Guitar': false}
+    vals = [true, false]
 
-    print(tree.eval(testDict))
+    for hornVal in vals:
+        for fluteVal in vals:
+            for guitarVal in vals:
+                testDict = {'Horn': hornVal, 'Flute': fluteVal, 'Guitar': guitarVal}
+                if wTree.eval(testDict) != tree.eval(testDict):
+                    print('\terror on ' + str(testDict))
+                    return
+                print('\t' + str(testDict) + ' -> ' + tree.eval(testDict))
 
 
-main()
+for i in range(0, 1000):
+    print('Attempt ' + str(i) + ':')
+    main()
